91dd9b0c572bc85aaf4587f35e04340d284a406a
shevakovshura <shevakov.shura@gmail.com>
2019-11-05 18:46:32 +0100

    First commit

---
+++ webapp/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+from webapp.model import db, News
+from webapp.weather import weather_by_city
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    @app.route('/')
+    def index():
+        page_title = "Новости Python"
+        weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
+        news_list = News.query.order_by(News.published.desc()).all()
+        return render_template('index.html',page_title=page_title, weather=weather, news_list=news_list)
+
+    return app
